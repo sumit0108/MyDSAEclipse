74a7563dfec4c99576e8382abb23acf2d87e4bce
sumit gond <you@example.com>
2022-11-06 10:08:13 +0530

    new commit

---
+++ DSAPractice/src/main/java/com/SinglyLinkedList/Node.java
@@ -0,0 +1,10 @@
+package com.SinglyLinkedList;
+
+public class Node {
+	
+	public int value;
+	
+	public Node next; //Because "next" is referring to the next Node address, that's why DATA-Type is Node
+
+	
+}
